f2917176f7956203dd56995531c9093c084717ea
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-03 23:19:26 -0500

    Create Employe schema for database

---
+++ server/models/Employee.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+const EmployeeSchema = new Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    tel:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    job:{
+        type: String,
+        required: true
+    },
+    eid:{
+        type: Number,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        required: true
+    },
+    updatedAt:{
+        type: Date,
+        required: true
+    },
+})
+
+EmployeeSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+});
+
+EmployeeSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
