99497d562cf8e2579756d2087ed797d1ffc8fb2e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 16:20:22 -0500

    Reformatted so database is connected before server starts

--- app.js
+++ app.js
@@ -7,50 +7,58 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 
 const connectDB = require('./server/config/db')
-connectDB()
+const startServer = async () => {
+    try {
+        await connectDB(); // Await for the database connection
 
-const app = express()
-const port = 5000 || process.env.PORT;
+        const app = express();
+        const port = process.env.PORT || 5000;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(methodOverride('_method'));
-
-
-// Static Files
-app.use(express.static('public'));
-
-// Express Session
-app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-        }
-    })
-)
-
-// Flash Message
-app.use(flash({sessionKeyName: 'flashMessage'}))
-
-// Templating Engine
-app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+        app.use(express.urlencoded({extended: true}));
+        app.use(express.json());
+        app.use(methodOverride('_method'));
 
 
-// Routes
-const employeeRouter = require('./server/routes/employee')
-app.use('/', employeeRouter)
+        // Static Files
+        app.use(express.static('public'));
 
-// Handle 404
-app.get('*', (req, res) => {
-    res.status(404).render('404')
-})
+        // Express Session
+        app.use(
+            session({
+                secret: 'secret',
+                resave: false,
+                saveUninitialized: true,
+                cookie: {
+                    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+                }
+            })
+        )
+
+        // Flash Message
+        app.use(flash({sessionKeyName: 'flashMessage'}))
+
+        // Templating Engine
+        app.use(expressLayout);
+        app.set('layout', './layouts/main');
+        app.set('view engine', 'ejs');
 
 
-app.listen(port, ()=>{
-    console.log("Server started!")
-})
+        // Routes
+        const employeeRouter = require('./server/routes/employee')
+        app.use('/', employeeRouter)
+
+        // Handle 404
+        app.get('*', (req, res) => {
+            res.status(404).render('404')
+        })
+
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}!`);
+        });
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
+    }
+};
+
+startServer();
+
